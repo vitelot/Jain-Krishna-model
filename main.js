15b526842b8ed424f4d69f0d87e3dd590111dc08
Vito DP Servedio <v.servedio@gmail.com>
2018-02-26 16:03:36 +0100

    added pool selection; in-out-degree selection; parameter explanation; pause button style; bar and node mouseover

--- main.js
+++ main.js
@@ -4,13 +4,14 @@ function main() {
   var timer;
 
   var nr_nodes = 50; // number of nodes
-  var m = .5/nr_nodes; // Erdos-Renyi parameter
+  var Kin = 0.5; // average in-degree
   var rate = 500;
+  var m = Kin/(nr_nodes-1);
   var pool_min = 5; // number of nodes to extract the minimum population
 
   var width = 600,
       height = 600,
-      node_radius = 2;
+      node_radius = 3;
 
   var force, nodes=[], links=[], node, link;
   var eigval, Matrix;
@@ -18,17 +19,14 @@ function main() {
   var eigv = new Array(nr_nodes);
   eigv = eigv.fill(1./nr_nodes);
 
+//// set the canvas dimensions
+  d3.select("#wrapper")
+    .style("width", 25 + width*4/3 + "px");
+
 //// for the parameter panel
   d3.select("#parameters")
-//  .attr("width", width)
+  .style("width", width/3 + "px")
   .style("height", height+"px");
-
-  d3.select("#rateValue").on("input", function() {
-    rate = +this.value;
-    clearInterval(timer);
-    if(runningFlag) timer = setInterval(run, rate);
-    d3.select("#rateLabel").text(rate+"ms");
-  });
 
 //// for the simulation
   var svg = d3.select("#simulation").append("svg")
@@ -89,7 +87,9 @@ var x_eigvect = d3.scale.ordinal().rangeRoundBands([0, width_eigvect],0.1),
             //.style("fill", "steelblue")
             .attr("y", function(d) { return y_eigvect(d); })
             .attr("width", x_eigvect.rangeBand())
-            .attr("height", function(d) { return height_eigvect - y_eigvect(d); });
+            .attr("height", function(d) { return height_eigvect - y_eigvect(d); })
+            .on("mouseover", mouseoverbar)
+            .on("mouseout", mouseoutbar);
 
 //// now the axes
     g_eigvect.append("g")
@@ -130,83 +130,10 @@ var x_eigvect = d3.scale.ordinal().rangeRoundBands([0, width_eigvect],0.1),
     //pieDynamics();
   }
 
-  function modelDynamics() {
-    //var n1 = Math.floor(Math.random()*nr_nodes), n2;
-    //var obj;
-    var i;
-    var maxscale;
-    var vector=[];
-
-    findPerron();
-    maxscale = d3.max(eigv);
-    if(maxscale>0) {
-      y_eigvect.domain([0, maxscale]);
-    }
-    d3.select(".axis--y")
-    .attr("transform", "translate("+ x_eigvect(0) +","+ y_eigvect(maxscale) + ")")
-    .call(d3.svg.axis().scale(y_eigvect).orient("left"));
-
-    g_eigvect.selectAll(".bar")
-          .data(eigv)
-          .transition()
-          .duration(0.7*rate)
-            .attr("y", function(d) { return y_eigvect(d); })
-            .attr("height", function(d) { return height_eigvect - y_eigvect(d); });
-
-    for(i=0; i<eigv.length; i++) {
-      vector.push({idx:i, val:eigv[i]});
-    }
-    //vector = eigv.slice(); // trick to copy an array by value
-    vector = vector.sort(function(a, b){return a.val - b.val}).splice(0,pool_min);
-    var rnd_item = vector[Math.floor(Math.random()*vector.length)].idx;
-
-//// implementation of Jain-Krishna
-    //links = links.filter( i => (i.source.idx !== rnd_item && i.target.idx !== rnd_item));
-    // cannot do filter since the vector links is bound to the force mechanism
-    i=0; do {
-        if(links[i].source.idx == rnd_item || links[i].target.idx == rnd_item) {
-          links.splice(i,1);
-          i--;
-        }
-        i++;
-    } while(i<links.length);
-
-    for (i=0,j=rnd_item; i<nr_nodes; i++) {
-        Matrix[i][j] = Matrix[j][i] = 0;
-        if(i!==j && Math.random()<m) {
-          links.push({source: i, target: j});
-          Matrix[j][i] = 1; // from i to j
-        }
-        if(i!==j && Math.random()<m) {
-          links.push({source: j, target: i});
-          Matrix[i][j] = 1; // from i to j
-        }
-    }
-////
-    // do {
-    //   n2 = Math.floor(Math.random()*nr_nodes);
-    // } while(n1 == n2);
-    //
-    // obj = {source: n1, target: n2};
-    //
-    // for (i=0; i< links.length; i++) {
-    //   if( (links[i].source.idx == n1 && links[i].target.idx == n2)){
-    //       //||(links[i].source.idx == n2 && links[i].target.idx == n1)) {
-    //     break;
-    //   }
-    // }
-    // if( i<links.length) {
-    //   links.splice(i,1);
-    //   Matrix[n1][n2] = 0;
-    // } else {
-    //   links.push(obj);
-    //   Matrix[n2][n1] = 1;
-    // }
-
-    restart();
-  }
-
+/////******************/////
+/////******************/////
   function startup() {
+/////******************/////
 
     activateButtons();
 
@@ -267,6 +194,89 @@ var x_eigvect = d3.scale.ordinal().rangeRoundBands([0, width_eigvect],0.1),
         .append("svg:title")
         .text(function(d,i){return i;});
 
+        node.on("mouseover", mouseovernode)
+        .on("mouseout", mouseoutnode);
+
+  }
+
+/////******************/////
+/////******************/////
+  function modelDynamics() {
+/////******************/////
+
+    var i;
+    var maxscale;
+    var vector=[];
+
+    findPerron();
+    maxscale = d3.max(eigv);
+    if(maxscale>0) {
+      y_eigvect.domain([0, maxscale]);
+    }
+    d3.select(".axis--y")
+    .attr("transform", "translate("+ x_eigvect(0) +","+ y_eigvect(maxscale) + ")")
+    .call(d3.svg.axis().scale(y_eigvect).orient("left"));
+
+    g_eigvect.selectAll(".bar")
+          .data(eigv)
+          .transition()
+          .duration(0.7*rate)
+            .attr("y", function(d) { return y_eigvect(d); })
+            .attr("height", function(d) { return height_eigvect - y_eigvect(d); });
+
+    for(i=0; i<eigv.length; i++) {
+      vector.push({idx:i, val:eigv[i]});
+    }
+    //vector = eigv.slice(); // trick to copy an array by value
+
+// select a random element among the pool_min least populated nodes
+    vector = vector.sort(function(a, b){return a.val - b.val}).splice(0,pool_min);
+    var rnd_item = vector[Math.floor(Math.random()*vector.length)].idx;
+
+//// implementation of Jain-Krishna
+    //links = links.filter( i => (i.source.idx !== rnd_item && i.target.idx !== rnd_item));
+    // cannot do filter since the vector links is bound to the force mechanism
+    i=0; do {
+        if(links[i].source.idx == rnd_item || links[i].target.idx == rnd_item) {
+          links.splice(i,1);
+          i--;
+        }
+        i++;
+    } while(i<links.length);
+
+    for (i=0,j=rnd_item; i<nr_nodes; i++) {
+        Matrix[i][j] = Matrix[j][i] = 0;
+        if(i!==j && Math.random()<m) {
+          links.push({source: i, target: j});
+          Matrix[j][i] = 1; // from i to j
+        }
+        if(i!==j && Math.random()<m) {
+          links.push({source: j, target: i});
+          Matrix[i][j] = 1; // from i to j
+        }
+    }
+////
+    // do {
+    //   n2 = Math.floor(Math.random()*nr_nodes);
+    // } while(n1 == n2);
+    //
+    // obj = {source: n1, target: n2};
+    //
+    // for (i=0; i< links.length; i++) {
+    //   if( (links[i].source.idx == n1 && links[i].target.idx == n2)){
+    //       //||(links[i].source.idx == n2 && links[i].target.idx == n1)) {
+    //     break;
+    //   }
+    // }
+    // if( i<links.length) {
+    //   links.splice(i,1);
+    //   Matrix[n1][n2] = 0;
+    // } else {
+    //   links.push(obj);
+    //   Matrix[n2][n1] = 1;
+    // }
+
+    restart();
   }
 
   function tick() {
@@ -350,15 +360,76 @@ var x_eigvect = d3.scale.ordinal().rangeRoundBands([0, width_eigvect],0.1),
         .on("click", restartSym);
     ////////////////////
     // inputs //
-      d3.select("#mValue").on("input", function() {
-        m = +this.value;
+      d3.select("#mValue")
+        .on("input", function() {
+            Kin = +this.value;
+            m = Kin/(nr_nodes-1);
       });
-      d3.select("#rateValue").on("input", function() {
-        rate = +this.value;
-        clearInterval(timer);
-        if(runningFlag) timer = setInterval(run, rate);
+
+      d3.select("#poolValue")
+        .on("input", function() {
+            pool_min = +this.value;
       });
+
+      d3.select("#rateValue")
+        .on("input", function() {
+          rate = +this.value;
+          clearInterval(timer);
+          if(runningFlag) timer = setInterval(run, rate);
+        });
     ////////////
+  }
+
+  function mouseovernode() {
+    var id = d3.select(this).datum().idx;
+
+    d3.select("#bar-" + id)
+        .style("stroke", "black");
+        //.attr("fill-opacity", "1.0");
+
+    d3.select(this).transition()
+        .duration(400)
+        .attr("r", 10);
+  }
+
+  function mouseoutnode() {
+    var id = d3.select(this).datum().idx;
+
+    d3.select("#bar-"+id)
+        .style("stroke", "white");
+        //.attr("fill-opacity", "0.8");
+
+    d3.select(this).transition()
+        .duration(400)
+        .attr("r", node_radius);
+  }
+
+  function mouseoverbar() {
+    var id = d3.select(this).attr("id");
+    var idx = id.match(/\d+/)[0];
+
+    d3.select("#node-" + idx)
+        .style("fill", "green")
+        .transition()
+          .duration(400)
+          .attr("r", 10);
+
+    d3.select(this)
+        .style("fill", "green");
+  }
+
+  function mouseoutbar() {
+    var id = d3.select(this).attr("id");
+    var idx = id.match(/\d+/)[0];
+
+    d3.select("#node-"+idx)
+        .style("fill", "#f99")
+        .transition()
+          .duration(400)
+          .attr("r", node_radius);
+
+        d3.select(this)
+            .style("fill", "steelblue");
   }
 
 }
